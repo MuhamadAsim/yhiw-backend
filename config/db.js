84000e0b714a50e94847e91d3330dfcffad08e63
Muhammad Asim <rao1235251325@gmail.com>
2026-02-27 00:22:11 +0500

    auth tokken

--- config/db.js
+++ config/db.js
@@ -13,4 +13,7 @@ const connectDB = async () => {
   }
 };
 
+
+//MONGO_URI=mongodb+srv://Kiosk:kiosk@cluster0.kacqumb.mongodb.net/Outfit?retryWrites=true&w=majority&appName=Cluster0/yhiw
+
 export default connectDB;
